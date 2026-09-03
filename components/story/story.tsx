21fbc795f858f829623c98cca320500745cbcece
Caleb <calebnull88@outlook.com>
2026-09-03 09:59:21 -0500

    Fixed variable error

--- components/story/story.tsx
+++ components/story/story.tsx
@@ -24,7 +24,7 @@ export function Story({ stats }: { stats: WrappedStats }) {
         e.preventDefault()
         next()
       } else if (e.key === "ArrowLeft") {
-        e.preventDefault
+        e.preventDefault()
         prev()
       }
     }
