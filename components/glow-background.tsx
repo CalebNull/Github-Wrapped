9d135e9087a5286c28788e2c994d95423595a8aa
Caleb <calebnull88@outlook.com>
2026-09-07 13:41:09 -0500

    Changed colors

--- components/glow-background.tsx
+++ components/glow-background.tsx
@@ -4,10 +4,10 @@ export function GlowBackground() {
       aria-hidden
       className="pointer-events-none fixed inset-0 -z-10 overflow-hidden bg-background"
     >
-      {/* soft glow */}
-      <div className="w-[32vmax]bun absolute inset-0 m-auto flex h-[32vmax] items-center justify-center">
-        <div className="aurora-blob h-full w-[50%] rounded-full bg-aurora-4 opacity-40 blur-[80px]" />
-      </div>
+      <div className="absolute -top-[10%] -left-[10%] h-[45vmax] w-[45vmax] rounded-full bg-aurora-1 opacity-60 blur-[90px]" />
+      <div className="absolute -top-[15%] -right-[10%] h-[40vmax] w-[40vmax] rounded-full bg-aurora-2 opacity-50 blur-[100px]" />
+      <div className="absolute -bottom-[15%] left-[25%] h-[38vmax] w-[38vmax] rounded-full bg-aurora-3 opacity-55 blur-[110px]" />
+      <div className="absolute inset-0 left-[40%] m-auto h-[30vmax] w-[30vmax] rounded-full bg-aurora-4 opacity-40 blur-[80px]" />
     </div>
   )
 }
